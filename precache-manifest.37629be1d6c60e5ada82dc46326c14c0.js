1e2ab8272f564ed80d709f41a9cac76800119e7d
Natosha <nmsuri28@gmail.com>
2019-04-09 20:29:42 -0500

    Updates

---
+++ precache-manifest.37629be1d6c60e5ada82dc46326c14c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/simpleweatherapp/static/media/background.b6796c73.jpg"
+  },
+  {
+    "revision": "596f58756966ba89cc43",
+    "url": "/simpleweatherapp/static/js/runtime~main.f1f7c0ab.js"
+  },
+  {
+    "revision": "b4be8cf733deec651cb4",
+    "url": "/simpleweatherapp/static/js/main.35ac592c.chunk.js"
+  },
+  {
+    "revision": "a65f59a93b26c2de44ac",
+    "url": "/simpleweatherapp/static/js/2.bbac7bca.chunk.js"
+  },
+  {
+    "revision": "b4be8cf733deec651cb4",
+    "url": "/simpleweatherapp/static/css/main.11d7294f.chunk.css"
+  },
+  {
+    "revision": "e72178054276ff1c639f3d80eb4dd9d6",
+    "url": "/simpleweatherapp/index.html"
+  }
+];
